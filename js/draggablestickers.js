4ab36f0288fba792fa650447b6d6c1b94113de01
A.C <creitzalex@gmail.com>
2026-02-21 22:04:57 -0500

    Add homepage and its assets

---
+++ js/draggablestickers.js
@@ -0,0 +1,51 @@
+var offset = 0;
+var mydivs = document.getElementsByClassName("draggable");
+for (var i = 0; i < mydivs.length; i++) {
+    dragElement(mydivs[i]);
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+    if (elmnt.getElementsByClassName("mydivheader")[0]) {
+
+    elmnt.getElementsByClassName(
+        "mydivheader"
+    )[0].onmousedown = dragMouseDown;
+    } else {
+    
+    elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    
+    document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+    
+    document.onmouseup = null;
+    document.onmousemove = null;
+    }
+}
---
+++ js/music-data.js
@@ -0,0 +1,27 @@
+let songs = [
+    {
+        name: 'PUSH UR T3MPRR',
+        path: 'https://files.catbox.moe/dlxxep.mp3',
+        artist: 'Femtanyl',
+        cover: '/assets/homepage/music/chaser.jpg'
+    },
+    {
+        name: 'Untouchable',
+        path: 'https://files.catbox.moe/7cue37.mp3',
+        artist: 'Golemm',
+        cover: '/assets/homepage/music/hazardousbubblebasics.jpg'
+    },
+    {
+        name: 'Krystle (URL Cyber Palace Mix)',
+        path: 'https://files.catbox.moe/l5nppt.mp3',
+        artist: 'Machine Girl',
+        cover: '/assets/homepage/music/wlfgrl.png'
+    },
+    {
+        name: 'Ghost',
+        path: 'https://files.catbox.moe/429t96.mp3',
+        artist: 'Machine Girl',
+        cover: '/assets/homepage/music/wlfgrl.png'
+    },
+
+]
