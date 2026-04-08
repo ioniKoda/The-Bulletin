b3d9e189be0d483dcb39115906d728d8de9df209
A.C <creitzalex@gmail.com>
2026-04-08 11:00:47 -0400

    Fixed issue with paths

--- public/js/music-data.js
+++ public/js/music-data.js
@@ -3,25 +3,25 @@ let songs = [
         name: 'PUSH UR T3MPRR',
         path: 'https://files.catbox.moe/dlxxep.mp3',
         artist: 'Femtanyl',
-        cover: '/assets/homepage/music/chaser.jpg'
+        cover: '../assets/homepage/music/chaser.jpg'
     },
     {
         name: 'Untouchable',
         path: 'https://files.catbox.moe/7cue37.mp3',
         artist: 'Golemm',
-        cover: '/assets/homepage/music/hazardousbubblebasics.jpg'
+        cover: '../assets/homepage/music/hazardousbubblebasics.jpg'
     },
     {
         name: 'Krystle (URL Cyber Palace Mix)',
         path: 'https://files.catbox.moe/l5nppt.mp3',
         artist: 'Machine Girl',
-        cover: '/assets/homepage/music/wlfgrl.png'
+        cover: '../assets/homepage/music/wlfgrl.png'
     },
     {
         name: 'Ghost',
         path: 'https://files.catbox.moe/429t96.mp3',
         artist: 'Machine Girl',
-        cover: '/assets/homepage/music/wlfgrl.png'
+        cover: '../assets/homepage/music/wlfgrl.png'
     },
 
 ]
